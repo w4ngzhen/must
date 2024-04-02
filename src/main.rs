0c96a13e911212ae85d111a0e82a1d1da79638c9
w4ngzhen <w4ngzhen@hotmail.com>
2024-04-02 13:04:44 +0800

    feat: 使用文字显示效果更好，且支持更多unicode的字体。

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod screen;
 mod constants;
 
 /// 开发运行前，请确保编译目录（理论上为target目录）下debug目录中，must可执行程序同级下有resources目录，
-/// 且目录中有字体文件："Consolas-with-Yahei Nerd Font.ttf"
+/// 且目录中有字体文件："DejaVuSansMono YaHei NF"
 /// 该字体文件请从项目目录/resources/目录下复制到对应目录
 fn main() {
     // 建立游戏客户端
@@ -20,7 +20,7 @@ fn main() {
             .expect("Could not create ggez context!");
     ctx.gfx.add_font(
         FONT_FLAG_NAME,
-        graphics::FontData::from_path(&ctx, "/Consolas-with-Yahei Nerd Font.ttf").expect("load font error"),
+        graphics::FontData::from_path(&ctx, "/DejaVuSansMono YaHei NF.ttf").expect("load font error"),
     );
     let my_game = GameState::new(&mut ctx);
     // Run!
